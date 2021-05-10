9e310d490d4bc44fde67e4cd4cd52a940fdb28de
JohanRiv <johancamiloandres@hotmail.com>
2021-05-10 12:49:44 -0500

    Ajuste reto parte 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FirstPage());
+  runApp(MyApp());
 }
 
-class FirstPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -13,39 +13,63 @@ class FirstPage extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Navigate to challenge Page'),
-          ),
-          body: Center(
-            child: TextButton(
-              child: Text('Nav to challenge'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SecondPage()));
-              },
-            ),
-          )),
+      routes: {
+        '/': (context) => FirstPage(),
+        '/challenge': (context) => SecondPage()
+      },
     );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Navigate to challenge Page'),
+        ),
+        body: Center(
+          child: TextButton(
+            child: Text('Nav to challenge!'),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondPage()));
+              print('Button clic');
+            },
+          ),
+        ));
   }
 }
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('Ejecución de page');
     return Scaffold(
       appBar: AppBar(
         title: Text('Reto de JohanRiv - Share'),
       ),
       body: Center(
         child: Stack(
+          alignment: Alignment.center,
           children: [
             Container(
+              alignment: Alignment.center,
               child: Image.network(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+                  'https://previews.123rf.com/images/archon7th/archon7th1901/archon7th190100126/123356127-comic-space-with-stars-funny-astronauts-and-spaceships-seamless-pattern.jpg'),
             ),
             Container(
-              child: Text('Naughty'),
+              height: 30,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[Colors.black45, Colors.black45],
+              )),
+              child: Text('Naughty',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 20)),
             )
           ],
         ),
